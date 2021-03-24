0871832e9b91aaf158560f51b8394ad0468064ed
swimmingsprite <f.chomjak@gmail.com>
2021-03-24 13:51:03 +0100

    Added generated classes

---
+++ src/main/java/com/swimmingsprite/xmlservice/XmlserviceApplication.java
@@ -0,0 +1,14 @@
+package com.swimmingsprite.xmlservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XmlserviceApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/swimmingsprite/xmlservice/XmlserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swimmingsprite.xmlservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
