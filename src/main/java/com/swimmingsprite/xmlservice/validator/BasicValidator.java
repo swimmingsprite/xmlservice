3e961eea3e0c3727509ef5b129325ef8678c340b
swimmingsprite <f.chomjak@gmail.com>
2021-03-24 14:27:43 +0100

    Validator impl

---
+++ src/main/java/com/swimmingsprite/xmlservice/validator/BasicValidator.java
@@ -0,0 +1,31 @@
+package com.swimmingsprite.xmlservice.validator;
+
+import org.springframework.stereotype.Component;
+import org.xml.sax.SAXException;
+
+import javax.xml.XMLConstants;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+
+@Component
+public class BasicValidator implements Validator {
+    private String xsdPath;
+
+    @Override
+    public boolean validate(String xml) {
+        try {
+            SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            Schema schema = schemaFactory.newSchema(new File(xsdPath));
+            javax.xml.validation.Validator validator = schema.newValidator();
+            validator.validate(new StreamSource(new ByteArrayInputStream(xml.getBytes(StandardCharsets.UTF_8))));
+            return true;
+        }
+        catch (Exception e) {
+            return false;
+        }
+    }
+}
--- src/main/java/com/swimmingsprite/xmlservice/validator/Validator.java
+++ src/main/java/com/swimmingsprite/xmlservice/validator/Validator.java
@@ -1,5 +1,8 @@
 package com.swimmingsprite.xmlservice.validator;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public interface Validator {
     boolean validate(String xml);
 }
