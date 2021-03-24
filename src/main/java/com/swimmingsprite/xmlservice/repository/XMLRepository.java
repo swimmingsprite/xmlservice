4a14d19059b40fa5fa63d924845ff3dd95b73e9f
swimmingsprite <f.chomjak@gmail.com>
2021-03-24 16:54:15 +0100

    XMLRepository created

---
+++ src/main/java/com/swimmingsprite/xmlservice/repository/XMLRepository.java
@@ -0,0 +1,12 @@
+package com.swimmingsprite.xmlservice.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class XMLRepository {
+
+    public void save() {
+
+    }
+
+}
