3dbb925bc2438a0517cb4d9a9d250d322e3cc482
swimmingsprite <f.chomjak@gmail.com>
2021-03-25 12:11:46 +0100

    added transformToHtml method

--- src/main/java/com/swimmingsprite/xmlservice/service/XMLService.java
+++ src/main/java/com/swimmingsprite/xmlservice/service/XMLService.java
@@ -2,6 +2,7 @@ package com.swimmingsprite.xmlservice.service;
 
 import com.swimmingsprite.xmlservice.ElementExtractor;
 import com.swimmingsprite.xmlservice.Parser;
+import com.swimmingsprite.xmlservice.XMLTransformer;
 import com.swimmingsprite.xmlservice.entity.invoice.DocumentType;
 import com.swimmingsprite.xmlservice.repository.GeneralRepository;
 import com.swimmingsprite.xmlservice.repository.InvoiceRepository;
@@ -19,28 +20,46 @@ public class XMLService {
     private final ValidatorFactory validatorFactory;
     private final Parser parser;
     private final ApplicationContext context;
+    private final XMLTransformer xmlTransformer;
 
 
     // TODO: 24. 3. 2021 fetch from file
     private final Map<String, Class<?>> namespaceClasses = new ConcurrentHashMap<>(
             Map.of("http://www.example.com/Invoice", DocumentType.class));
 
-    public XMLService(ValidatorFactory validatorFactory, Parser parser, ApplicationContext context) {
+    public XMLService(ValidatorFactory validatorFactory, Parser parser, ApplicationContext context, XMLTransformer transformer) {
         this.validatorFactory = validatorFactory;
         this.parser = parser;
         this.context = context;
+        this.xmlTransformer = transformer;
     }
 
     public void save(String xml) {
-        String namespace = new ElementExtractor(xml).getNamespace();
+        ElementExtractor elementExtractor = new ElementExtractor(xml);
+        String namespace = elementExtractor.getNamespace();
         if (namespace == null) throw new NoSuchElementException("Namespace not present.");
         Class<?> type = namespaceClasses.get(namespace);
         if (type == null) throw new RuntimeException("There's no proper pojo class to map for "+namespace);
         Validator validator = validatorFactory.getInstance(namespace);
         if (validator.validate(xml)) {
-            Object object = parser.parse(xml, type);
+//            Object object = parser.parse(xml, type);
+            Object object = parser.parse(elementExtractor.getDocument(), type);
             getRepository(type).save(object);
             return;
+        }
+        System.err.println("validation failed...");
+        throw new RuntimeException(String.format("XML with namespace %s is not valid.", namespace));
+    }
+
+    public String transformToHTML(String xml) {
+        ElementExtractor elementExtractor = new ElementExtractor(xml);
+        String namespace = elementExtractor.getNamespace();
+        if (namespace == null) throw new NoSuchElementException("Namespace not present.");
+        Class<?> type = namespaceClasses.get(namespace);
+        if (type == null) throw new RuntimeException("There's no proper pojo class to map for "+namespace);
+        Validator validator = validatorFactory.getInstance(namespace);
+        if (validator.validate(xml)) {
+            return xmlTransformer.transform(xml, stylesheet);
         }
         System.err.println("validation failed...");
         throw new RuntimeException(String.format("XML with namespace %s is not valid.", namespace));
