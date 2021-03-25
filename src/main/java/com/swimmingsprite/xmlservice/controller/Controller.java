6d28978a3759f9abdf001a1ac49e6129596d8539
swimmingsprite <f.chomjak@gmail.com>
2021-03-25 16:23:23 +0100

    transform to html controller

--- src/main/java/com/swimmingsprite/xmlservice/controller/Controller.java
+++ src/main/java/com/swimmingsprite/xmlservice/controller/Controller.java
@@ -1,11 +1,14 @@
 package com.swimmingsprite.xmlservice.controller;
 
 import com.swimmingsprite.xmlservice.service.XMLService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 public class Controller {
@@ -20,6 +23,18 @@ public class Controller {
         try {
             service.save(xml);
             return ResponseEntity.ok().build();
+        }
+        catch (Exception e) {
+            printException(e);
+            return ResponseEntity.status(500).build();
+        }
+    }
+
+    @PostMapping(value = "/transformToHtml", consumes = "application/xml", produces = "text/html")
+    public ResponseEntity<String> transformToHtml(@RequestBody String xml) {
+        try {
+            String html = service.transformToHTML(xml);
+            return new ResponseEntity<>(html, HttpStatus.OK);
         }
         catch (Exception e) {
             printException(e);
