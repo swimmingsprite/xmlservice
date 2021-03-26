3e153eeede76ac212174dd32d0669513b85737cd
swimmingsprite <f.chomjak@gmail.com>
2021-03-26 13:59:06 +0100

    todo

--- src/main/java/com/swimmingsprite/xmlservice/repository/InvoiceRepository.java
+++ src/main/java/com/swimmingsprite/xmlservice/repository/InvoiceRepository.java
@@ -1,8 +1,16 @@
 package com.swimmingsprite.xmlservice.repository;
 
 import com.swimmingsprite.xmlservice.entity.invoice.DocumentType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Repository;
+
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.internet.MimeMessage;
 
 @Repository
 public class InvoiceRepository implements GeneralRepository {
@@ -17,5 +25,7 @@ public class InvoiceRepository implements GeneralRepository {
         DocumentType doc = (DocumentType) obj;
         System.out.println(doc.getBank().getBankName());
         System.out.println(doc.getCustomer().getCompanyName());
+        // TODO: 26. 3. 2021 IMPLEMENTATION
+
     }
 }
