11dd3f272e69917aaba7d019d5c80bc3ad865f6e
swimmingsprite <f.chomjak@gmail.com>
2021-03-26 13:59:33 +0100

    created mail service

---
+++ src/main/java/com/swimmingsprite/xmlservice/service/MailService.java
@@ -0,0 +1,66 @@
+package com.swimmingsprite.xmlservice.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
+
+@Component
+public class MailService {
+    JavaMailSender mailSender;
+
+    MailService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendHtml(String to, String msg, String subject) {
+     /*   SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("swisp@pepisandbox.com");
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(msg);
+        mailSender.send(message);
+
+*/
+
+        /*try {
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+            String htmlMsg = msg;
+            mimeMessage.setContent(htmlMsg, "text/html");
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setFrom("swisp@pepisandbox.com");
+            mailSender.send(mimeMessage);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }*/
+
+
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom("swisp@pepisandbox.com");
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText("V prílohe Vám posielam faktúru. ");
+
+            helper.addAttachment("Faktura.html", new ByteArrayResource(msg.getBytes(StandardCharsets.UTF_8)));
+            mailSender.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+
+    }
+}
