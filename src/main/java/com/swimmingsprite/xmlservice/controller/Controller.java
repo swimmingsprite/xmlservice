0dfaa73cab948721d9121d98b394c581f6d364e1
swimmingsprite <f.chomjak@gmail.com>
2021-03-26 13:56:08 +0100

    transform to html and send method

--- src/main/java/com/swimmingsprite/xmlservice/controller/Controller.java
+++ src/main/java/com/swimmingsprite/xmlservice/controller/Controller.java
@@ -3,11 +3,9 @@ package com.swimmingsprite.xmlservice.controller;
 import com.swimmingsprite.xmlservice.service.XMLService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.Email;
 import java.util.Optional;
 
 @RestController
@@ -23,27 +21,40 @@ public class Controller {
         try {
             service.save(xml);
             return ResponseEntity.ok().build();
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             printException(e);
             return ResponseEntity.status(500).build();
         }
     }
 
     @PostMapping(value = "/transformToHtml", consumes = "application/xml", produces = "text/html")
-    public ResponseEntity<String> transformToHtml(@RequestBody String xml) {
+    public ResponseEntity<String> transformToHtml(@RequestBody String xml,
+                                                  @RequestParam("variant") String variant) {
         try {
-            String html = service.transformToHTML(xml);
+            String html = service.transformToHTML(xml, variant);
             return new ResponseEntity<>(html, HttpStatus.OK);
+        } catch (Exception e) {
+            printException(e);
+            return ResponseEntity.status(500).build();
         }
-        catch (Exception e) {
+    }
+
+    @PostMapping(value = "/transformToHtmlAndSend", consumes = "application/xml", produces = "text/html")
+    public ResponseEntity<String> transformToHtmlAndSend(
+            @RequestBody String xml,
+            @RequestParam("variant") String variant,
+            @Email @RequestParam("email") String email) {
+        try {
+            service.transformToHTMLAndSend(xml, variant, email);
+            return ResponseEntity.ok(null);
+        } catch (Exception e) {
             printException(e);
             return ResponseEntity.status(500).build();
         }
     }
 
     private void printException(Exception e) {
-        System.err.println("Message: "+e.getMessage()+"\n"+"Cause: "+e.getCause());
+        System.err.println("Message: " + e.getMessage() + "\n" + "Cause: " + e.getCause());
     }
 
 
